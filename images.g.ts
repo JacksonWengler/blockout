8a7cb0e14f46f1e7992057b591dc3198ca0f5813
JacksonWengler <jwengler4304@stu.neisd.net>
2025-02-28 11:33:39 -0600

    Added part 2!

--- images.g.ts
+++ images.g.ts
@@ -41,6 +41,63 @@ namespace myImages {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
+            case "image3":
+            case "block1":return img`
+2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 
+2 3 3 3 3 2 4 4 4 4 4 4 4 4 4 3 
+2 2 2 2 2 2 4 3 3 3 3 3 3 3 4 3 
+3 3 7 7 7 7 4 3 3 3 3 3 3 3 4 3 
+3 3 7 3 3 7 4 4 4 4 4 4 4 4 4 3 
+3 3 7 3 3 7 b b b b b b 3 f f f 
+3 3 7 3 3 7 b 3 3 3 3 b 3 f 3 f 
+3 3 7 7 7 7 b 3 3 3 3 b 3 f 3 f 
+6 6 6 6 6 6 b 3 3 3 3 b 3 f 3 f 
+6 3 3 3 3 6 b 3 3 3 3 b 3 f 3 f 
+6 6 6 6 6 6 b b b b b b 3 f 3 f 
+3 b b b b b b b b b b b b f 3 f 
+3 b 3 3 3 3 3 3 3 3 3 3 b f 3 f 
+3 b b b b b b b b b b b b f f f 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+`;
+            case "image4":
+            case "block2":return img`
+7 7 7 7 8 7 2 2 2 2 2 2 8 2 2 2 
+2 8 8 8 8 8 8 8 2 2 2 2 8 2 2 2 
+8 2 2 2 8 2 7 2 8 2 2 2 8 2 2 2 
+8 2 2 2 8 2 7 2 8 2 2 2 8 2 2 2 
+8 9 9 9 8 9 9 9 8 2 2 2 8 2 2 2 
+8 2 2 2 8 2 7 2 8 9 9 2 8 2 2 2 
+8 2 2 2 2 8 8 8 8 4 4 4 4 4 4 4 
+8 2 2 2 2 2 7 2 4 2 2 2 9 2 2 2 
+8 7 7 7 7 7 2 4 8 2 2 2 9 2 2 2 
+2 8 8 8 8 8 4 8 2 2 2 2 2 9 2 2 
+2 2 d d d d 4 2 2 2 2 2 2 9 2 2 
+2 d 2 2 2 2 4 d 2 2 2 2 2 9 2 2 
+2 d 2 2 2 2 4 d 2 2 2 2 2 9 2 2 
+2 d 2 2 2 2 4 d 2 2 2 2 2 9 2 2 
+2 d 2 2 2 2 4 d 2 2 2 2 2 9 2 2 
+2 d 2 2 2 2 4 d 2 2 2 2 2 9 2 2 
+`;
+            case "image5":
+            case "block3":return img`
+e e e e e e e e e e e e e e e e 
+3 e e 2 e e e e e e e e e e c e 
+3 e e e 2 2 e e e e e e e c 4 4 
+e 3 8 e e e 2 e e e e 9 4 4 e e 
+e 3 e 8 e e e 2 2 e 4 4 e e e e 
+e 3 e e 8 8 e 4 4 4 c 9 e e e e 
+e e 3 e e 4 4 5 e c 2 2 e e e e 
+e e 3 4 4 e 5 8 c e e 9 2 e e e 
+e e e 3 e 5 e c 8 8 e e e 2 2 e 
+e e e 3 e e c e e e 8 e e e e 2 
+e e e 5 3 e e e e e e e e e e e 
+e e 5 e 3 e e e e e e e e 9 9 9 
+e e e c 3 e e e 9 e e 9 9 e e e 
+e e e e e 3 9 9 e e e e e e e e 
+e e e e e 3 e e e e e e e e e e 
+e e e e e e 3 e e e e e e e e e 
+`;
         }
         return null;
     })
